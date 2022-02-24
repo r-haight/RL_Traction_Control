483dfbe79a908384c631d90c19e9500653d0e5b8
rachelhaighton <rachelhaighton@cmail.carleton.ca>
2022-02-24 11:37:28 -0500

    scenario with comments updated

--- model_class_tester.py
+++ model_class_tester.py
@@ -4,18 +4,18 @@ import matplotlib.pyplot as plt
 #step 1: create a model object
 # state = current, angular velocity of wheel, forward velocity of body
 state = [0,0,0] #start with 1rad/s and 1m/s
-max_iterations = 700
+max_iterations = 1000
 tire = model.traction_model(state)
 v=5
 #step 2: call iterate for a max # iterations
 for i in range(max_iterations):
     tire.iterate(v) #input the voltage as a parameter
-    if(i == 150): # at 150 we hit a snow patch
-        v = 3.3
+    if(i == 150):
+        v = 10
         pass
-    elif(i == 300): # at 200 we hit a wet patch of road
-        tire.road_condition_status = 2
-    elif(i>400):
+    elif(i == 300): # at 300 we hit an icy patch of road
+         tire.road_condition_status = 4
+    elif(i>800):
         #hit the brakes
         tire.w = 0
 
